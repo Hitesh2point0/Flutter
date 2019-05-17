484756d2ba2bd27886f3300c997638f1d789eaa3
hitesh <hitesh@zestlabs.in>
2019-05-17 19:43:05 +0530

    Tried Step 16

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'dart:io' show Platform;
 
 void main() => runApp(MyApp());
 
@@ -6,24 +10,58 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Scaffold(
-        body: HomePage(),
-      ),
+        return MaterialApp(
+          theme: ThemeData(
+            brightness: Brightness.light
+          ),
+    
+        home: Scaffold(
+          body: Center(
+            child: new CoolBox(),
+          ),
+        ),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({
+class CoolBox extends StatefulWidget {
+  const CoolBox({
     Key key,
   }) : super(key: key);
 
   @override
+  _CoolBoxState createState() => _CoolBoxState();
+}
+
+class _CoolBoxState extends State<CoolBox> {
+
+  double width = 100;
+  double height = 100;
+  Color color = Colors.green;
+
+  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        child: Text('Hello World'),
-      ),
+    return AnimatedContainer(
+      duration: Duration(seconds: 1),
+      curve: Curves.elasticInOut,
+      
+      color: color,
+      width: width,
+      height: height,
+      child: FlatButton(
+        child: Text('Random'),
+        onPressed: (){
+          setState(() {
+           width = Random().nextDouble() * 500;
+           height = Random().nextDouble() * 500;
+
+           int r = Random().nextInt(225);
+           int g = Random().nextInt(225);
+           int b = Random().nextInt(225);
+           color = Color.fromRGBO(r, g, b, 1);
+          });
+        }
+        ),
     );
   }
 }
