22e819d6d15fac2f23dde27409f5ef7da1f73239
HiteshKumar2995 <50194089+Hitesh2point0@users.noreply.github.com>
2019-05-17 12:30:21 +0530

    Flutter Default Code
    All Set Dependency Applied

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Scaffold(
+        body: HomePage(),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        child: Text('Hello World'),
+      ),
+    );
+  }
+}
